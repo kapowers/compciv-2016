958cdf6781b42b194be311b64364b64a7ad1c183
Kristen <kapowers@stanford.edu>
2016-02-17 23:43:33 -0800

    sorted

--- exercises/0013-sorted-names/e.py
+++ exercises/0013-sorted-names/e.py
@@ -2,15 +2,15 @@ import os
 filename = os.path.join("tempdata","ssa-babynames-nationwide-2014.txt")
 
 namesdict = {}
-myitems = namesdict.items()
-mylist = list(myitems)
-
 for line in open(filename,'r'):	
 	name, sex, babies = line.strip().split(',')
 	if namesdict.get(name):
 		namesdict[name] += int(babies)
 	else:
 		namesdict[name] = int(babies)
+
+myitems = namesdict.items()
+mylist = list(myitems)
 
 allbabies = 0
 for foo in mylist:
@@ -24,7 +24,19 @@ begin = 0
 for baby in anotherlist[0:10]:
 	begin += baby[1]
 print("Names 1 to 10: ", round(begin * 100/allbabies, 1))
-	#print("Names 11 to 100: ")
-	#print("Names 101 to 1000: ")
-	#print("Names 1001 to 100000: ")
-	#print("Names 10001 to 30579: ")
+begin = 0
+for baby in anotherlist[10:100]:
+	begin += baby[1]
+print("Names 11 to 100: ", round(begin * 100/allbabies,1))
+begin = 0
+for baby in anotherlist[100:1000]:
+	begin += baby[1]
+print("Names 101 to 1000: ",round(begin * 100/allbabies,1))
+begin = 0
+for baby in anotherlist[1000:10000]:
+	begin += baby[1]
+print("Names 1001 to 100000: ",round(begin * 100/allbabies,1))
+begin = 0
+for baby in anotherlist[10000:30579]:
+	begin += baby[1]
+print("Names 10001 to 30579: ",round(begin * 100/allbabies,1))
