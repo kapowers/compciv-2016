d7c9bf5f47410ca410c679e7089b2d1086293dbb
Kristen <kapowers@stanford.edu>
2016-02-29 12:08:32 -0800

    watson

--- homework/fetcher.py
+++ homework/fetcher.py
@@ -3,7 +3,9 @@ from os import makedirs
 PICS_DIR = 'pics'
 makedirs(PICS_DIR, exist_ok = True)
 
-URLS = 
+URLS = #Code didn't save originally and can't find location of all 5 images I used. However,
+#I would list URL of each image (not hyperlink) here 
+
 
 for url in URLS:
     print("Downloading", url)
--- homework/printout.py
+++ homework/printout.py
@@ -10,14 +10,19 @@ htmlfile.write("<html><title>Whatsup Watson</title><body>")
 htmlfile.write("<h1>Analysis by Kristen Powers</h1>")
 
 for jsonname in glob(join(RECOG_DIR, '*.json')):
-    print("Extracting", jsonname)
-    j = json.load(open(jsonname))
-    img = j['images'][0]
-    imgname = img['image']
-    htmlfile.write("<h2>%s</h2>" % imgname)
+	print("Extracting", jsonname)
+	j = json.load(open(jsonname))
+	img = j['images'][0]
+	imgname = img['image']
+	htmlfile.write("<h2>%s</h2>" % imgname)
+	imgfilename = join(PICS_DIR, imgname)
+	htmlfile.write('<img src="%s">' % imgfilename)
 
-    imgfilename = join(PICS_DIR, imgname)
-
-    htmlfile.write('<img src="%s">' % imgfilename)
+	imagedict = j['images'][0] 
+	scores = imagedict['scores']
+	for s in scores: 
+		print(s['classifier_id'])
+		print(s['score'])
+	htmlfile.write("<h2>%s</h2>" % scores)
 
 htmlfile.close()
