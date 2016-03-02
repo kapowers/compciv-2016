df4948749ebe2801d4de0c6e541f86295d2922f9
Kristen <kapowers@stanford.edu>
2016-03-01 16:15:28 -0800

    werk

---
+++ exercises/0020-gender-detector/a.py
@@ -0,0 +1,21 @@
+
+import requests
+from os import makedirs
+from os.path import join
+from shutil import unpack_archive
+from glob import glob
+SOURCE_URL = 'https://www.ssa.gov/oact/babynames/names.zip'
+DATA_FOLDER = 'tempdata'
+DATA_ZIP_PATH = join(DATA_FOLDER, 'names.zip')
+
+makedirs(DATA_FOLDER, exist_ok=True)
+
+print("Downloading", SOURCE_URL)
+resp = requests.get(SOURCE_URL)
+with open(DATA_ZIP_PATH, 'wb') as f:
+    f.write(resp.content)
+
+unpack_archive(DATA_ZIP_PATH, extract_dir=DATA_FOLDER)
+
+babynamenames = glob(join(DATA_DIR, '*.txt'))
+print("There are", len(babynamenames), 'txt files')
--- homework/fetcher.py
+++ homework/fetcher.py
@@ -3,14 +3,12 @@ from os import makedirs
 PICS_DIR = 'pics'
 makedirs(PICS_DIR, exist_ok = True)
 
-URLS = #Code didn't save originally and can't find location of all 5 images I used. However,
-#I would list URL of each image (not hyperlink) here 
-
+URLS = {'https://farm2.staticflickr.com/1638/24902500570_25c1894293_z_d.jpg', 'https://farm2.staticflickr.com/1467/23941893710_937ac8952a_z_d.jpg', 'https://farm2.staticflickr.com/1692/23609238704_70b48736d7_z_d.jpg' , 'https://farm2.staticflickr.com/1659/23869644609_6b02d41866_z_d.jpg','https://farm2.staticflickr.com/1632/23610659283_fa9936f7d7_z_d.jpg','https://farm2.staticflickr.com/1720/24237481605_8761572254_z_d.jpg'}
 
 for url in URLS:
     print("Downloading", url)
     resp = requests.get(url)
-    fname = 
+    fname = 'pics'
     print("Saving to", fname)
     with open(fname, 'wb') as w:
         w.write(resp.content)
