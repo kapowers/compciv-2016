406a780fb81b94e0cb3b5ef64b13452d47b0438c
Kristen <kapowers@stanford.edu>
2016-03-11 15:59:17 -0800

    gender

--- projects/gender-detector/wrangle_data.py
+++ projects/gender-detector/wrangle_data.py
@@ -9,18 +9,20 @@ DATA_PATH = join(DATA_DIR, 'names.csv')
 WRANGLED_DIR = 'tempdata/wrangled'
 WRANGLED_HEAD = ['year', 'name', 'description']
 WRANGLED_DATA_PATH = join(WRANGLED_DIR, 'wranglednames.csv')
+datarows = list()
 
 with open(DATA_PATH, 'r', encoding='latin1') as thatfile:
-	for line in thatfile:
-		year, name, description = line.split(',', 2)
-		datarows = list(csv.DictReader(thatfile))
+	reader = csv.DictReader(thatfile)
+	for row in thatfile:
+		datarows.append(row)
 
 yofile = open(WRANGLED_DATA_PATH, 'w')
 yocsv = DictWriter(yofile, fieldnames=WRANGLED_HEAD)
 yocsv.writeheader()
 
 mylist = []
-for year, name, description in datarows:
+for line in datarows:
+	year, name, description = line.split(',', 2)
 	xdict = {'year': year, 'name': name, 'description': description}
 	mylist.append(xdict)
 
